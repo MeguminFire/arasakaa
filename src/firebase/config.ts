2d9e58990b4492fd96f09161e66fb76c5bf9a0e6
Kenji Kaname <gmorecj22@gmail.com>
2026-02-10 01:38:52 +0000

    tell me my firebase api that i can put on vercel, currently at Environme

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 // This configuration is automatically injected by the framework.
 // For local development, you can add your own configuration here.
 export const firebaseConfig = {
-  apiKey: "AIzaSyAJOuTWjuFRW6-gfRWHO1y16Vk-pag_H-0",
-  authDomain: "studio-6157369367-cb19f.firebaseapp.com",
-  projectId: "studio-6157369367-cb19f",
-  storageBucket: "studio-6157369367-cb19f.appspot.com",
-  messagingSenderId: "847721265732",
-  appId: "1:847721265732:web:afc07f35bf84feb3b51a11",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAJOuTWjuFRW6-gfRWHO1y16Vk-pag_H-0",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-6157369367-cb19f.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-6157369367-cb19f",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-6157369367-cb19f.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "847721265732",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:847721265732:web:afc07f35bf84feb3b51a11",
 };
